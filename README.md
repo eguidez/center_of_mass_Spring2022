ea8f4d9f6a54f8f0dfca3baa62bbcef93bbb016f
Emilie Guidez <emilie.guidez@ucdenver.edu>
2022-05-10 14:49:09 -0600

    Added functions to the .py file and completed the README file and added the output of the test file water_dimer_output

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # This code computes the xyz coordinates of the center of mass of two molecules
 ## Author: Emilie Guidez Date: 04/2022
+
+INPUT: The .xyz file containing the coordinates of the molecules.
+       The atomic_mass file containing the atomic mass in amu of the atoms in the molecules
+OUTPUT: file named output_center_of_mass containing the x,y,z coordinates of the center of mass of each molecules
+
+EXECUTION: To execute the code enter: python dimer_geometries.py
+As prompted, enter the number of atoms in each molecule and the name of the .xyz file containing the coordinates.
+
+TEST_INPUT: The water.xyz file contains the coordinates of a water dimer in Angstrom.
+TEST_OUTPUT (water_dimer_output):
+Molecule 1 center of mass coordinates:  -1.5192235062480295 -0.056766187419790295 0.0
+Molecule 2 center of mass coordinates: 1.387526052619776 0.05717480447264767 0.0
--- dimer_geometries.py
+++ dimer_geometries.py
@@ -9,117 +9,133 @@
 import os
 import numpy as np
 #Input files
-file_name = 'water_dimer.xyz'
-file_path = os.path.abspath(file_name)
-file = open(file_path,'r')
-data = file.readlines()
-file.close()
-#print(data)
+def read_file(file_name):
+    """
+    Reads file_name text file and returns the content in a data list
+    INPUT: file_name
+    OUTPUT: data
+    """
+    file_path = os.path.abspath(file_name)
+    file = open(file_path,'r')
+    data = file.readlines()
+    file.close()
+    return data
 
-atom_file_name = 'atomic_mass'
-atom_file_path = os.path.abspath(atom_file_name)
-atom_file = open(atom_file_path,'r')
-atom_mass_data = atom_file.readlines()
-atom_file.close()
-#print(atom_mass_data)
+def assign_data(data):
+    """
+    Assign x,y,z coordinates and atom names from data to their respective array.
+    INPUT: data array generated from reading an xyz file
+    OUTPUT: x_coords, y_coords, z_coords, atom_names arrays
+    """
+    atom_names = np.empty([0])
+    x_coords = np.empty([0])
+    y_coords = np.empty([0])
+    z_coords = np.empty([0])
+    for linenum,line in enumerate(data):
+        if (linenum == 0):
+            total_atom_num = int(line)
+        elif (linenum == 1):
+            comment_line = line
+        else:
+            atom_data = line.split()
+            atom_names = np.append(atom_names,atom_data[0])
+            x_coords = np.append(x_coords,float(atom_data[1]))
+            y_coords = np.append(y_coords,float(atom_data[2]))
+            z_coords = np.append(z_coords,float(atom_data[3]))
+# Generate atom_mass array here
+    atom_mass = atom_mass_assign(atom_names)
+    return (x_coords, y_coords, z_coords, atom_names,atom_mass)
 
-#Input data
-atoms_molecule1 = 3
-atoms_molecule2 =3
-min_distance = 2.0  #Angstrom
-max_distance = 3.0  #Angstrom
-interval = 0.1 #A
-
-# Assign data from the files
-atom_names = np.empty([0])
-x_coords = np.empty([0])
-y_coords = np.empty([0])
-z_coords = np.empty([0])
-for linenum,line in enumerate(data):
-    if (linenum == 0):
-        total_atom_num = int(line)
-    elif (linenum == 1):
-        comment_line = line
-    else:
-        atom_data = line.split()
-        atom_names = np.append(atom_names,atom_data[0])
-        x_coords = np.append(x_coords,float(atom_data[1]))
-        y_coords = np.append(y_coords,float(atom_data[2]))
-        z_coords = np.append(z_coords,float(atom_data[3]))
-#print(x_coords)
-#print(atom_names)
-#Get the mass of each atom
-atom_mass = np.empty([0])
-for i in range(0,len(atom_names)):
-    for atom in atom_mass_data :
-        atom_split = atom.split()
+def atom_mass_assign(atom_names):
+    """
+    Assign atomic mass in amu to each atom in atom_names array
+    INPUT: Array atom_names containing the atomic symbols of the molecules
+    OUTPUT: Array atom_mass containing the mass of each atom in the molecules
+    """
+    atom_mass = np.empty([0])
+    for i in range(0,len(atom_names)):
+        for atom in atom_mass_data :
+            atom_split = atom.split()
 #        print('atom_split',atom_split)
-        if atom_names[i] == atom_split[0]:
-            atom_mass = np.append(atom_mass,float(atom_split[1]))
-#print(atom_mass)
+            if atom_names[i] == atom_split[0]:
+                atom_mass = np.append(atom_mass,float(atom_split[1]))
+    return (atom_mass)
 
-x_coords_1 = x_coords[0:atoms_molecule1]
-y_coords_1 = y_coords[0:atoms_molecule1]
-z_coords_1 = z_coords[0:atoms_molecule1]
-atom_names_1 = atom_names[0:atoms_molecule1]
-atom_mass_1 = atom_mass[0:atoms_molecule1]
-x_coords_2 = x_coords[atoms_molecule1:total_atom_num]
-y_coords_2 = y_coords[atoms_molecule1:total_atom_num]
-z_coords_2 = z_coords[atoms_molecule1:total_atom_num]
-atom_names_2 = atom_names[atoms_molecule1:total_atom_num]
-atom_mass_2 = atom_mass[atoms_molecule1:total_atom_num]
-#print(atom_names_1)
-#print(atom_names_2)
-#print(atom_mass_1)
-#print(x_coords_1)
-sum_mass_times_x_1 =0 
-sum_mass_times_y_1 =0 
-sum_mass_times_z_1 =0 
-mass_sum_1 = 0
-#Calculate the center of mass of each molecule
-for i in range (0,atoms_molecule1):
-    mass_times_x_1 = atom_mass_1[i] * x_coords_1[i]
-    mass_times_y_1 = atom_mass_1[i] * y_coords_1[i]
-    mass_times_z_1 = atom_mass_1[i] * z_coords_1[i]
-    sum_mass_times_x_1 =sum_mass_times_x_1 + mass_times_x_1
-    sum_mass_times_y_1 =sum_mass_times_y_1 + mass_times_y_1
-    sum_mass_times_z_1 =sum_mass_times_z_1 + mass_times_z_1
-    mass_sum_1 = mass_sum_1 + atom_mass_1[i]
+def data_per_molecule(array,atom_number_array,molecule_number):
+    """
+    Slices array to only get the data for one molecule
+    INPUT: array to slice (array), array containing number of atoms in
+    each molecules (atom_number_array) and molecule number
+    OUTPUT: array_for_one_molecule
+    """
+    if molecule_number == 0:
+        start = 0
+        end = int(atom_number_array[molecule_number])
+    else :
+        start = int(atom_number_array[molecule_number])
+        end =  int(atom_number_array[0]) + int(atom_number_array[1])
+    array_for_one_molecule = array[start:end]
+    return (array_for_one_molecule)
 
-com_x_molecule1 = sum_mass_times_x_1/mass_sum_1
-com_y_molecule1 = sum_mass_times_y_1/mass_sum_1
-com_z_molecule1 = sum_mass_times_z_1/mass_sum_1
-print(com_x_molecule1,com_y_molecule1,com_z_molecule1)
+def center_of_mass(x_coords,y_coords,z_coords,atom_mass):
+    """
+    Calculates center of mass coordinates of a molecule
+    INPUT: x_coordinates (x_coords),y_coordinates(y_coord),
+        z_coordinate(z_coord),atom masses (atom_mass)
+    OUTPUT: com_x,com_y,com_z
+    """
+    sum_mass_times_x =0
+    sum_mass_times_y =0
+    sum_mass_times_z =0
+    mass_sum = 0
+#Calculate the center of mass of the molecule
+    for i in range (0,len(atom_mass)):
+        mass_times_x = atom_mass[i] * x_coords[i]
+        mass_times_y = atom_mass[i] * y_coords[i]
+        mass_times_z = atom_mass[i] * z_coords[i]
+        sum_mass_times_x =sum_mass_times_x + mass_times_x
+        sum_mass_times_y =sum_mass_times_y + mass_times_y
+        sum_mass_times_z =sum_mass_times_z + mass_times_z
+        mass_sum = mass_sum + atom_mass[i]
 
-sum_mass_times_x_2 =0 
-sum_mass_times_y_2 =0 
-sum_mass_times_z_2 =0 
-mass_sum_2 = 0
-for i in range (0,atoms_molecule2):
-    mass_times_x_2 = atom_mass_2[i] * x_coords_2[i]
-    mass_times_y_2 = atom_mass_2[i] * y_coords_2[i]
-    mass_times_z_2 = atom_mass_2[i] * z_coords_2[i]
-    sum_mass_times_x_2 =sum_mass_times_x_2 + mass_times_x_2
-    sum_mass_times_y_2 =sum_mass_times_y_2 + mass_times_y_2
-    sum_mass_times_z_2 =sum_mass_times_z_2 + mass_times_z_2
-    mass_sum_2 = mass_sum_2 + atom_mass_2[i]
+    com_x = sum_mass_times_x/mass_sum
+    com_y = sum_mass_times_y/mass_sum
+    com_z = sum_mass_times_z/mass_sum
+    return(com_x,com_y,com_z)
 
-com_x_molecule2 = sum_mass_times_x_2/mass_sum_2
-com_y_molecule2 = sum_mass_times_y_2/mass_sum_2
-com_z_molecule2 = sum_mass_times_z_2/mass_sum_2
-print(com_x_molecule2,com_y_molecule2,com_z_molecule2)
+def write_output(output_name,com_coords,molecule_number):
+    """
+    Write the center of masss coordinates of the molecule to output_name
+    INPUT: output file name (output_name) and xyz coords of the center of mass
+    OUTPUT: output file(output_name) containing xyz coords of the center of mass
+    """
+    output = open(output_name,'a')
+    string = 'Molecule ' + str(molecule_number+1) + \
+    ' center of mass coordinates: '+' '+ str(com_coords[0]) +' ' \
+    + str(com_coords[1]) +' '+ str(com_coords[2])+'\n'
+    output.write(string)
+    output.close()
 
-output_name = 'output_center_of_mass'
-output = open(output_name,'w')
-string1 = 'Molecule 1 center of mass coordinates: '+' '+ str(com_x_molecule1) +' '+ str(com_y_molecule1) +' '+ str(com_z_molecule1)+'\n'
-string2 = 'Molecule 2 center of mass coordinates:'+' '+ str(com_x_molecule2) +' '+ str(com_y_molecule2) +' '+ str(com_z_molecule2)
-output.write(string1)
-output.write(string2)
-output.close()
+if __name__ == '__main__':
+#Input data from the user
+    atoms_molecule1 = input("Enter the number of atoms in molecule 1: ")
+    atoms_molecule2 = input("Enter the number of atoms in molecule 2: ")
+    file_name = input("Enter the name of the .xyz file containing the atom coordinates: ")
+    output_name = input("Enter the name of the file containing the output ")
 
+#Array containing the number of atoms in each molecule
+    atom_number_array = np.array([atoms_molecule1, atoms_molecule2])
 
-# In[ ]:
-
-
-
-
+#Now extract data from the files
+    xyz_data = read_file(file_name)
+    atom_mass_data = read_file('atomic_mass')
+    all_x, all_y, all_z, all_atom_names,all_atom_mass = assign_data(xyz_data)
+    for molecule in range (0,len(atom_number_array)):
+        x_coords = data_per_molecule(all_x,atom_number_array,molecule)
+        y_coords = data_per_molecule(all_y,atom_number_array,molecule)
+        z_coords = data_per_molecule(all_z,atom_number_array,molecule)
+        atom_names = data_per_molecule(all_atom_names,atom_number_array,molecule)
+        atom_mass = data_per_molecule(all_atom_mass,atom_number_array,molecule)
+        com_coordinates = center_of_mass(x_coords,y_coords,z_coords,atom_mass)
+        write_output(output_name,com_coordinates,molecule)
+    print('The program terinated normally')
